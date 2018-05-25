07b41dd6cf5b7eb0d40b2a67f1381f83395fc71d
Damian <themrdamus@gmail.com>
2018-05-25 22:39:18 +0100

    added bootstrap css

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => (
   <Router>
     <div>
 {/* Debug Menu  */}
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
       <ul>
         <li>
           <Link to="/">Home</Link>
