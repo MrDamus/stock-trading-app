9b940f7100f56628671b37170e85c07bdf2e9929
Damian <themrdamus@gmail.com>
2018-05-29 16:08:54 +0100

    hover + popover

--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import { Popover, ButtonToolbar, OverlayTrigger , Button} from 'react-bootstrap'
+
+const popoverHoverFocus = (
+  <Popover id="suggestionsHover" title="Holy guacamole!">
+    <strong>It works!</strong> Check this information.
+  </Popover>
+);
 
 const Suggestions = ({ companies, onSelect }) => {
   const options = companies.map(company => (
-    <li 
+    <ul 
       onClick={() => onSelect(company.symbol)} 
       key={company.symbol}>
-      {`${company.symbol}: ${company.name}`}
-    </li>
+      <ButtonToolbar>
+
+      <OverlayTrigger
+        trigger={['hover', 'focus']}
+        placement="bottom"
+        overlay={popoverHoverFocus}
+        >
+        <Button>{`${company.symbol}: ${company.name}`}</Button>
+      </OverlayTrigger>
+      </ButtonToolbar>
+    </ul>
+
   ))
   return <ul>{options}</ul>
 }
+
 
 Suggestions.propTypes = {
   companies: PropTypes.arrayOf(PropTypes.shape({ name : String, symbol: String })),
