2ccd6546ec5bdf328775a8c084c94a99ed25dc46
Damian <themrdamus@gmail.com>
2018-05-31 22:57:20 +0100

    reworking logging in

--- src/Login.js
+++ src/Login.js
@@ -1,34 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, FormGroup, FormControl, ControlLabel, InputGroup } from "react-bootstrap";
+import { FormGroup, FormControl, ControlLabel, InputGroup } from "react-bootstrap";
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { loginSuccess } from './actions'
+import { loginSuccess, inputName } from './actions'
+import LoginCreateClearButtons from './Login/loginCreateClearButtons'
 
 const serverUrl = 'http://localhost:8080/users/'
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inputValue: '',
-      query: '',
-      price: '',
-      chart: [],
-      user: {
-        name: "" || "xyz",
-        email: "" || "xyz@gmail.com",
-        password: "" || "xyz"
-      }
-    };
-    this.validateForm = this.validateForm.bind(this);
+    // this.validateForm = this.validateForm.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     this.setState({
-      user: {...this.state.user, [event.target.id] : event.target.value}
+      user: { ...this.state.user, [event.target.id]: event.target.value }
     });
   }
 
@@ -36,100 +26,100 @@ class Login extends Component {
     event.preventDefault();
   }
 
-  handleErrors(response) {
-    if (!response.ok) {
-      throw response.json();
-    }
-    return response;
-}
+  // handleErrors(response) {
+  //   if (!response.ok) {
+  //     throw response.json();
+  //   }
+  //   return response;
+  // }
 
-  validateForm() {
-    return this.state.user.email.length > 0 && this.state.user.password.length > 0;
-  }
+  // validateForm() {
+  //   return this.state.user.email.length > 0 && this.state.user.password.length > 0;
+  // }
 
-  login() {
-      fetch(`http://localhost:8080/users/${this.state.user.email}`, {
-          method: 'POST',
-          body: JSON.stringify({ password: this.state.user.password }),
-          headers: {
-            'content-type': 'application/json'
-          },
-      })
-      .then((data) => data.json())
-      .then(data => {
-        console.warn(data)
-        this.props.loginSuccess(data)
-      })
-      .then(() => this.props.history.push('/profile'))
-      .catch(e => alert(e))
-  }
+  // login() {
+  //   fetch(`http://localhost:8080/users/${this.state.user.email}`, {
+  //     method: 'POST',
+  //     body: JSON.stringify({ password: this.state.user.password }),
+  //     headers: {
+  //       'content-type': 'application/json'
+  //     },
+  //   })
+  //     .then((data) => data.json())
+  //     .then(data => {
+  //       console.warn(data)
+  //       this.props.loginSuccess(data)
+  //     })
+  //     .then(() => this.props.history.push('/profile'))
+  //     .catch(e => alert(e))
+  // }
 
-  createNewUser(user) {
-    return fetch(serverUrl, {
-        method: 'POST',
-        body: JSON.stringify(user),
-        headers: {
-          'content-type': 'application/json'
-        },
-    })
-    .then(this.handleErrors)
-    .then((data) => data.json())
-    .then(() => this.props.history.push('/profile'))
-    .catch(e => {
-      Promise.resolve(e)
-      .then(a => alert(a.error))
-    })
-  }
+  // createNewUser(user) {
+  //   return fetch(serverUrl, {
+  //     method: 'POST',
+  //     body: JSON.stringify(user),
+  //     headers: {
+  //       'content-type': 'application/json'
+  //     },
+  //   })
+  //     .then(this.handleErrors)
+  //     .then((data) => data.json())
+  //     .then(() => this.props.history.push('/profile'))
+  //     .catch(e => {
+  //       Promise.resolve(e)
+  //         .then(a => alert(a.error))
+  //     })
+  // }
 
-  clearDatabase(e) {
-    fetch(serverUrl, {
-        method: 'DELETE',
-        body: e,
-    })
-    console.log('All records from database has been cleared.')
-  }
+  // clearDatabase(e) {
+  //   fetch(serverUrl, {
+  //     method: 'DELETE',
+  //     body: e,
+  //   })
+  //   console.log('All records from database has been cleared.')
+  // }
 
   render() {
     return (
       <div className="Login" style={{ width: '100%' }}>
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="name" bsSize="large" style={{ width: '90%', margin: '0 auto' }}>
-              <ControlLabel>Name </ControlLabel>
-            <InputGroup style={{ width: '90%' }}>
+        <form onSubmit={this.handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
+          <FormGroup controlId="name" bsSize="large" style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+            <ControlLabel style={{textAlign: 'center'}}>Name </ControlLabel>
+            <InputGroup>
               <FormControl
-                style={{ width: '50%' }}
+                style={{ }}
                 autoFocus
                 type="name"
-                value={this.state.user.name}
-                onChange={this.handleChange}
+                value={this.props.name}
+                onChange={(event) => this.props.inputName(event.target.value)}
               />
             </InputGroup>
           </FormGroup>
-          <FormGroup controlId="email" bsSize="large" style={{ width: '90%', margin: '0 auto' }}>
-              <ControlLabel>Email </ControlLabel>
-            <InputGroup style={{ width: '90%' }}>
+          <FormGroup controlId="email" bsSize="large" style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+            <ControlLabel style={{textAlign: 'center'}}>Email </ControlLabel>
+            <InputGroup>
               <FormControl
-                style={{ width: '50%' }}
+                style={{ }}
                 autoFocus
                 type="email"
-                value={this.state.user.email}
-                onChange={this.handleChange}
+                value={this.props.email}
+                onChange={(event) => this.props.inputName(event.target.value)}
               />
             </InputGroup>
           </FormGroup>
-          <FormGroup controlId="password" bsSize="large" style={{ width: '90%', margin: '0 auto' }}>
-            <ControlLabel>Password </ControlLabel>
-            <InputGroup style={{ width: '90%' }}>
-            <FormControl
-              style={{ width: '50%' }}
-              value={this.state.user.password}
-              onChange={this.handleChange}
-              type="password"
-            />
+          <FormGroup controlId="password" bsSize="large" style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+            <ControlLabel style={{textAlign: 'center'}}>Password </ControlLabel>
+            <InputGroup>
+              <FormControl
+                style={{ }}
+                value={this.props.password}
+                onChange={this.handleChange}
+                type="password"
+              />
             </InputGroup>
           </FormGroup>
-          <div style={{width: '40%', margin: '0 auto'}}>
-
+          <LoginCreateClearButtons history={this.props.history} />
+          {/* <div style={{ width: '40%', margin: '0 auto' }}>
             <Button
               block
               bsSize="large"
@@ -137,42 +127,48 @@ class Login extends Component {
               disabled={!this.validateForm()}
               type="submit"
               onClick={() => this.login()}
-              >
+            >
               Login
             </Button>
-              </div>
-              <div style={{width: '40%', margin: '0 auto'}}>
-          <Button
-            block
-            bsSize="large"
-            bsStyle="primary"
-            disabled={!this.validateForm()}
-            type="submit"
-            onClick={()=> this.createNewUser(this.state.user)}
-          >
-            Create account
+          </div>
+          <div style={{ width: '40%', margin: '0 auto' }}>
+            <Button
+              block
+              bsSize="large"
+              bsStyle="primary"
+              disabled={!this.validateForm()}
+              type="submit"
+              onClick={() => this.createNewUser(this.state.user)}
+            >
+              Create account
           </Button>
           </div>
-          <div style={{width: '40%', margin: '0 auto'}}>
-          <Button
-            block
-            bsSize="large"
-            bsStyle="danger"
-            // type="submit"
-            onClick={() => this.clearDatabase()}
-          >
-            Clear database
+          <div style={{ width: '40%', margin: '0 auto' }}>
+            <Button
+              block
+              bsSize="large"
+              bsStyle="danger"
+              // type="submit"
+              onClick={() => this.clearDatabase()}
+            >
+              Clear database
           </Button>
-          </div>
+          </div> */}
         </form>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({loginForm}) => ({
+  name: loginForm.name,
+})
+
+
 // TODO: map dispatch to props, LOGIN
 const mapDispatchToProps = (dispatch) => {
   return {
+    inputName: (name) => dispatch(inputName(name)),
     loginSuccess: (user) => dispatch(loginSuccess(user)),
   }
 }
@@ -185,4 +181,4 @@ Login.defaultProps = {
   loginSuccess: 'Login has succeed'
 }
 
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
