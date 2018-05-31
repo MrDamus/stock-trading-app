4ebd24315647af506c31487e19e0aa299b98e17c
Damian <themrdamus@gmail.com>
2018-05-31 17:05:10 +0100

    rewritten styles in flexbox

--- src/CompanyInfo.js
+++ src/CompanyInfo.js
@@ -5,29 +5,29 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import StockAmountPicker from './companyInfo/stockAmountPicker'
 
-const CompanyInfo = ({price, symbol, companyName, chart }) => (
-      <div className="CompanyInfo" 
-      style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
-          <p style={{ margin: '0 auto'}}>
-            {`Latest price of ${symbol} ${companyName} is ${price}`}
-          </p>
-          <LineChart width={730} height={250} data={chart}
-          // margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-          style={{ margin: '0 auto'}}
-          >
-          <CartesianGrid strokeDasharray="5 5" />
-          <XAxis dataKey="date" />
-          <YAxis dataKey="open" />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="open" stroke="#8884d8" />
-          <Line type="monotone" dataKey="close" stroke="#8884d8" />
-        </LineChart>
-        <div>
-          <StockAmountPicker price={price}/>
-          </div>
-      </div>
-    );
+const CompanyInfo = ({ price, symbol, companyName, chart }) => (
+  <div className="CompanyInfo"
+    style={{ display: 'flex', flexDirection: 'column' }}>
+    <p style={{ alignSelf: 'center' }}>
+      {`Latest price of ${symbol} ${companyName} is ${price}`}
+    </p>
+    <LineChart width={730} height={250} data={chart}
+      // margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      style={{ alignSelf: 'center' }}
+    >
+      <CartesianGrid strokeDasharray="5 5" />
+      <XAxis dataKey="date" />
+      <YAxis dataKey="open" />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="open" stroke="#8884d8" />
+      <Line type="monotone" dataKey="close" stroke="#8884d8" />
+    </LineChart>
+    <div>
+      <StockAmountPicker price={price}/>
+    </div>
+  </div>
+);
 
 const mapStateToProps = ({stockData}) => ({
   price: stockData.details.latestPrice,
