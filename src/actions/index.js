9420aab17d54dfe7c30f323349d77899d320e579
Damian <themrdamus@gmail.com>
2018-06-03 18:42:42 +0100

    login function rewritten

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import makeTransaction from "../services/user";
+import userService from "../services/user";
 
 export const selectCompany = (payload) => ({
   type: 'SELECT_COMPANY',
@@ -7,6 +7,11 @@ export const selectCompany = (payload) => ({
 
 export const loginSuccess = (payload) => ({
   type: 'LOGIN_SUCCESS',
+  payload
+})
+
+export const loginError = (payload) => ({
+  type: 'LOGIN_ERROR',
   payload
 })
 
@@ -40,6 +45,16 @@ export const inputName = (payload) => ({
   payload
 })
 
+export const inputEmail = (payload) => ({
+  type: 'INPUT_EMAIL',
+  payload
+})
+
+export const inputPassword = (payload) => ({
+  type: 'INPUT_PASSWORD',
+  payload
+})
+
 
 export function buyStock() {
   return function (dispatch, getState) {
@@ -54,8 +69,8 @@ export function buyStock() {
       price: details.latestPrice,
       date: Date.now()
     }
-    //3rd parameter of this function is 100 and it's 'money'
-    return makeTransaction(user.email, transactionDetails, 100).then(
+    //3rd parameter of this function is 100 and it's 'money'??????
+    return userService.makeTransaction(user.email, transactionDetails, 100).then(
       getState => dispatch(buyStockSuccess(getState)),
       error => dispatch(buyStockError(error))
     );
@@ -75,9 +90,56 @@ export function sellStock() {
       date: Date.now()
     }
     
-    return makeTransaction(user.email, transactionDetails, 100).then(
+    return userService.makeTransaction(user.email, transactionDetails, 100).then(
       getState => dispatch(sellStockSuccess(getState)),
       error => dispatch(sellStockError(error))
     );
   };
+}
+
+const serverUrl = 'http://localhost:8080/users/'
+
+export function login() {
+  return function (dispatch, getState) {
+    const user  = getState().loginForm;
+    return userService.login(user.email, user.password).then(
+      getState => dispatch(loginSuccess(getState)) ,
+      error => {
+        dispatch(loginError(error))
+        throw new Error(error)
+      }
+    );
+  };
+}
+
+export function createNewUser(user) {
+  return fetch(serverUrl, {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then(this.handleErrors)
+    .then((data) => data.json())
+    .then(() => this.props.history.push('/profile'))
+    .catch(e => {
+      Promise.resolve(e)
+        .then(a => alert(a.error))
+    })
+}
+
+export function clearDatabase(e) {
+  fetch(serverUrl, {
+    method: 'DELETE',
+    body: e,
+  })
+  console.log('All records from database has been cleared.')
+}
+
+export function handleErrors(response) {
+  if (!response.ok) {
+    throw response.json();
+  }
+  return response;
 }
