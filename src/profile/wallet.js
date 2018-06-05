00863ec97da6faa175fb1b0653b5356110a6f676
Damian <themrdamus@gmail.com>
2018-06-05 16:36:23 +0100

    added title and changed styles

--- src/profile/wallet.js
+++ src/profile/wallet.js
@@ -5,26 +5,27 @@ import moment from 'moment';
 import { sellStock } from '../actions';
 import { Button } from 'react-bootstrap';
 
-
-
 const Wallet = (data) =>
-  (<div className='wallet' key={data.date} style={{ display: "flex", justifyContent: "space-around" }}>
-    <p>
-      {`You own: ${data.amount}
+  (<div className='wallet' key={data.date}>
+    <h3>Wallet</h3>
+    <ul style={{ display: "flex", justifyContent: "space-around" }}>
+      <p>
+        {`You own: ${data.amount}
    stock of ${data.symbol} ${data.companyName}
    company bought on ${moment(data.date).format('MMM DD h:mm A')}
    for ${data.price}$`}
-    </p>
-    <p>
-      <Button
-        bsSize="small"
-        bsStyle="warning"
-        // type="submit"
-        onClick={sellStock}
-      >
-        Sell
+      </p>
+      <p>
+        <Button
+          bsSize="small"
+          bsStyle="warning"
+          // type="submit"
+          onClick={sellStock}
+        >
+          Sell
   </Button>
-    </p>
+      </p>
+    </ul>
   </div>
   )
 
@@ -35,7 +36,7 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 Wallet.propTypes = {
-  
+
 };
 
 
