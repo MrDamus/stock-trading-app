19ba7eabd3acbadcf8e375dc882624799790c2b2
Damian <themrdamus@gmail.com>
2018-06-05 21:56:51 +0100

    fixed login form input components

--- src/Login.js
+++ src/Login.js
@@ -1,52 +1,17 @@
 import React from 'react';
 import './App.css';
-import { FormGroup, FormControl, ControlLabel, InputGroup } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { inputName, inputEmail, inputPassword } from './actions/loginForm'
 import Buttons from './Login/buttons'
-import LoginForm from './Login/loginForm'
+import FormInput from './Login/loginForm'
 
 const Login = ({inputName, inputEmail, inputPassword, name, email, password}) => (
       <div className="Login" style={{ width: '100%' }}>
         <form onSubmit={event => event.preventDefault()} style={{display: 'flex', flexDirection: 'column'}}>
-          {/* Create form input field component  zrobic jeden ogolny component i tylko props sie zmienia w zaleznosci co uzywa*/}
-          <LoginForm controlId='Name' controlLabel={inputName} type='name'/>
-          <LoginForm controlLabel={inputEmail}/>
-          <LoginForm controlLabel={inputPassword}/>          
-          {/* <FormGroup controlId="name" bsSize="large" style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
-            <ControlLabel style={{textAlign: 'center'}}>Name </ControlLabel>
-            <InputGroup>
-              <FormControl
-                autoFocus
-                type="name"
-                value={name}
-                onChange={(event) => inputName(event.target.value)}
-              />
-            </InputGroup>
-          </FormGroup>
-          <FormGroup controlId="email" bsSize="large" style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
-            <ControlLabel style={{textAlign: 'center'}}>Email </ControlLabel>
-            <InputGroup>
-              <FormControl
-                autoFocus
-                type="email"
-                value={email}
-                onChange={(event) => inputEmail(event.target.value)}
-              />
-            </InputGroup>
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large" style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
-            <ControlLabel style={{textAlign: 'center'}}>Password </ControlLabel>
-            <InputGroup>
-              <FormControl
-                autoFocus
-                value={password}
-                onChange={(event) => inputPassword(event.target.value)}
-                type="password"
-              />
-            </InputGroup>
-          </FormGroup> */}
+          <FormInput name="name" inputValue={name} onChange={(event) => inputName(event.target.value)} />
+          <FormInput name="email" inputValue={email} onChange={(event) => inputEmail(event.target.value)} />
+          <FormInput name="password" inputValue={password} onChange={(event) => inputPassword(event.target.value)} />
           <Buttons onClick={() => this.props.history.push('/profile')} />
         </form>
       </div>
@@ -55,7 +20,7 @@ const Login = ({inputName, inputEmail, inputPassword, name, email, password}) =>
 const mapStateToProps = ({loginForm}) => ({
   name: loginForm.name,
   email: loginForm.email,
-  password: loginForm.password,  
+  password: loginForm.password,
 })
 
 const mapDispatchToProps = (dispatch) => {
--- src/Login/loginForm.js
+++ src/Login/loginForm.js
@@ -1,38 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { FormGroup, FormControl, ControlLabel, InputGroup } from 'react-bootstrap';
-import { inputValue } from '../actions/index'
 
-const LoginForm = ({controlId, controlLabel, type, value, inputValue }) => (
-      <div className="Login" style={{ width: '100%' }}>
-        <form onSubmit={event => event.preventDefault()} style={{display: 'flex', flexDirection: 'column'}}>
-          <FormGroup controlId={controlId} bsSize="large" style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
-            <ControlLabel style={{textAlign: 'center'}}>{controlLabel} </ControlLabel>
-            <InputGroup>
-              <FormControl
-                autoFocus
-                type={type}
-                value={value}
-                onChange={(event) => inputValue(event.target.value)}
-              />
-            </InputGroup>
-          </FormGroup>
-        </form>
-      </div>
-    );
-
-const mapStateToProps = ({loginForm}) => ({
-  name: loginForm.name,
-  email: loginForm.email,
-  password: loginForm.password,  
-})
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    inputValue: (value) => dispatch(inputValue(value)),
-  }
-}
+const LoginForm = ({ name, inputValue, onChange }) => (
+  <FormGroup controlId={name} bsSize="large" style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+    <ControlLabel style={{textAlign: 'center'}}>{name} </ControlLabel>
+    <InputGroup>
+      <FormControl
+        autoFocus
+        type={name}
+        value={inputValue}
+        onChange={onChange}
+      />
+    </InputGroup>
+  </FormGroup>
+);
 
 LoginForm.propTypes = {
   inputName: PropTypes.func.isRequired,
@@ -40,4 +22,4 @@ LoginForm.propTypes = {
   inputPassword: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
+export default LoginForm;
