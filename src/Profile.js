d9bdd0c7fd272a32c66c7a99caf63d302dc8e36d
Damian <themrdamus@gmail.com>
2018-06-09 23:51:03 +0100

    reworked profile to pure function

--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { sellStock } from './actions/makeTransaction';
@@ -8,15 +8,13 @@ import GoogleShareButton from './profile/socialMediaButtons/googleShareButton'
 import TransactionHistory from './profile/transactionHistory'
 import Wallet from './profile/wallet'
 
-class Profile extends Component {
-  render() {
-    return (
+const Profile = ({ user }) => (
       <div className="Profile" style={{ width: '100%' }}>
-        <p>E-mail: {this.props.user.email}</p>
-        <p>Profile name: {this.props.user.name}</p>
-        <p>Money: {this.props.user.money}</p>
-        <Wallet data={this.props.user.wallet.filter(transaction => transaction.amount > 0)}/>
-        <TransactionHistory data={this.props.user.wallet.filter(transaction => transaction.sold)} />
+        <p>E-mail: {user.email}</p>
+        <p>Profile name: {user.name}</p>
+        <p>Money: {user.money}</p>
+        <Wallet data={user.wallet.filter(transaction => transaction.amount > 0)}/>
+        <TransactionHistory data={user.wallet.filter(transaction => transaction.sold)} />
         <div className="socialMediaButtons">
           <FacebookShareButton />
           <TwitterShareButton />
@@ -24,8 +22,6 @@ class Profile extends Component {
         </div>
       </div>
     );
-  }
-}
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -38,17 +34,7 @@ const mapStateToProps = ({ userData }) => ({
 })
 
 Profile.propTypes = {
-  name: PropTypes.string,
-  symbol: PropTypes.string,
-  email: PropTypes.string,
-  money: PropTypes.number
+  user: PropTypes.object.isRequired
 };
-
-Profile.defaultProps = {
-  name: 'Name',
-  symbol: 'company symbol',
-  email: 'email',
-  money: 0
-}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
