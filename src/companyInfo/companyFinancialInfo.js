a69bb7b11c2f86724d61fdfd79ae76f61ebe148c
Damian <themrdamus@gmail.com>
2018-06-14 19:44:22 +0100

    added fetch to get financial information

---
+++ src/companyInfo/companyFinancialInfo.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
+const CompanyFinancialInfo = ({ finances }) => (
+  <div style={{ alignSelf: 'center' }}>
+    {/* {finances.map((data) => 
+      Object.keys(data)
+        .filter(key => data[key]) 
+        .map((key, i) => <p key={data[key]+i} style={{ alignSelf: 'center' }}>{`${key}: ${data[key]}`}</p>))} */}
+  </div>
+)
+
+const mapStateToProps = ({ stockData }) => ({
+  finances: stockData.finances,
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+CompanyFinancialInfo.propTypes = {
+  finances: PropTypes.arrayOf(PropTypes.shape({
+    reportDate : String,
+    grossProfit: Number,
+    costOfRevenue: Number,
+    operatingRevenue: Number,
+    totalRevenue: Number,
+    operatingIncome: Number,
+    netIncome: Number,
+    researchAndDevelopment: Number,
+    operatingExpense: Number,
+    currentAssets: Number,
+    totalAssets: Number,
+    currentCash: Number,
+    currentDebt: Number,
+    totalCash: Number,
+    totalDebt: Number,
+    shareholderEquity: Number,
+    cashChange: Number,
+    cashFlow: Number
+  })),
+};
+
+CompanyFinancialInfo.defaultProps = {
+  grossProfit: '0',
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CompanyFinancialInfo);
