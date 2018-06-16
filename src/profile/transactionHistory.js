4d58ae43ce33e9bba2d533bc568789107dcfec7c
Damian <themrdamus@gmail.com>
2018-06-16 20:55:39 +0100

    updated transaction history

--- src/profile/transactionHistory.js
+++ src/profile/transactionHistory.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import TransactionHistoryListElement from './transactionHistoryListElement.js'
 
 const TransactionHistory = ({data}) =>
-  (<div className="transactionHistory" key={data.date} style={{ display: "flex", flexDirection: "column" }}>
+  (<div className="transactionHistory" style={{ display: "flex", flexDirection: "column" }}>
     <h3 style={{ alignSelf: 'center' }}>Transaction History</h3>
     <ul style={{ display: "flex", flexDirection: "column" }}>
-      {data.map(transaction => <TransactionHistoryListElement key={data.date} data={transaction} />)}
+      {data
+        ? data.map(transaction => <TransactionHistoryListElement key={data.date} data={transaction} />)
+        : null
+      }
     </ul>
   </div>
   )
