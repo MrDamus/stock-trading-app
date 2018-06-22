77a08e40a03d9ef4d9e75f5485b3b273b4c2d449
Damian <themrdamus@gmail.com>
2018-06-22 15:14:35 +0100

    Fixed endpoints

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const version = 'v1/'
 export const SERVER_URL = `http://localhost:8081/${version}`
-export const ENPOINTS = {
+export const ENDPOINTS = {
   PROFILE: 'users/profile',
   LOGIN: 'auth/login',
   REGISTER: 'auth/register',
